14719b5d54a65759a86e814e25836c214870f0e8
Max <99695664+MaxHard2017@users.noreply.github.com>
2025-12-18 01:00:19 +0300

    Добавлен update.sh для чистки и копирования нового задания

---
+++ update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+rm -rf ~/projects/gitrepos/1c/netology/1c_netology_tasks/in/*
+rm -rf ~/projects/gitrepos/1c/netology/1c_netology_tasks/out/*
+rm -rf ~/projects/gitrepos/1c/netology/1c_netology_tasks/infobase/*
+mv ~/downloads/*.md ~/projects/gitrepos/1c/netology/1c_netology_tasks/in
+mv ~/downloads/*.cf ~/projects/gitrepos/1c/netology/1c_netology_tasks/in
+mv ~/downloads/*.cfu ~/projects/gitrepos/1c/netology/1c_netology_tasks/in
+mv ~/downloads/*.dt ~/projects/gitrepos/1c/netology/1c_netology_tasks/in
+echo work directory updated
