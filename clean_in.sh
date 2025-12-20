b34036397d2d1629fdf874e1e4ba500aa9e85c98
Max <99695664+MaxHard2017@users.noreply.github.com>
2025-12-20 04:30:25 +0300

    1-5_1_itfirm Создание ролей и пользователей

---
+++ clean_in.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+rm -rf ~/projects/gitrepos/1c/netology/1c_netology_tasks/in/*
+mv ~/downloads/*.md ~/projects/gitrepos/1c/netology/1c_netology_tasks/in
+mv ~/downloads/*.cf ~/projects/gitrepos/1c/netology/1c_netology_tasks/in
+mv ~/downloads/*.cfu ~/projects/gitrepos/1c/netology/1c_netology_tasks/in
+mv ~/downloads/*.dt ~/projects/gitrepos/1c/netology/1c_netology_tasks/in
+echo work directory updated
