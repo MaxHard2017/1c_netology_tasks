cae4a36edaea9ae56f72def9595e5f96904f281a
Max <99695664+MaxHard2017@users.noreply.github.com>
2025-12-30 03:27:53 +0300

    в файле new.sh не удаляем файлы из папки in

--- new.sh
+++ new.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-rm -rf ~/projects/gitrepos/1c/netology/1c_netology_tasks/in/*
-rm -rf ~/projects/gitrepos/1c/netology/1c_netology_tasks/out/*
 rm -rf ~/projects/gitrepos/1c/netology/1c_netology_tasks/infobase/*
 mv ~/downloads/*.md ~/projects/gitrepos/1c/netology/1c_netology_tasks/in
 mv ~/downloads/*.cf ~/projects/gitrepos/1c/netology/1c_netology_tasks/in
